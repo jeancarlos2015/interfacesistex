a435e64c0d4b482849d1589220570dba1b9f9f65
jean <jean@gmail.com>
2017-11-15 19:58:13 -0200

    alteracao template

--- src/main/webapp/js/controllerUsuarios.js
+++ src/main/webapp/js/controllerUsuarios.js
@@ -1,26 +1,33 @@
 angular.module('appUsuario', [])
-.controller('controllerUsuarios', function($scope, $http) {
-    $scope.usuario = {};
-   $scope.salvarUsuario = function (){
-       $http.post('https://servicocontroleusuarios.herokuapp.com/usuario',$scope.usuario).
-        then(function(response) {
-            if(response.data){
-                $scope.mensagemUsuario = "Usuario cadastrado com sucesso!!!";
-            }else{
-                $scope.mensagemUsuario = "Use um cadastro diferente !!!";
-            }
+        .controller('controllerUsuarios', function ($scope, $http) {
+            $scope.usuario = {};
+            $scope.salvarUsuario = function () {
+                $http.post('https://servicocontroleusuarios.herokuapp.com/usuario', $scope.usuario).
+                        then(function (response) {
+                            if (response.data) {
+                                $scope.mensagemUsuario = "Usuario cadastrado com sucesso!!!";
+                            } else {
+                                $scope.mensagemUsuario = "Use um cadastro diferente !!!";
+                            }
+                        });
+            };
+            $scope.usuarioautent = {};
+            $scope.autenticarUsuario = function () {
+                $http.post('https://servicocontroleusuarios.herokuapp.com/usuario/autenticar', $scope.usuarioautent).
+                        then(function (response) {
+                            if (response.data) {
+                                //$scope.mensagemUsuario = response.data;
+                                $http.go("usuario"); //exemplo do .state abaixo
+
+                                .state("usuario", {
+                                    url: "/Usuario",
+                                    controller: "UsuarioCtrl",
+                                    templateUrl: "meu/caminho/usuario.html"
+                                })
+                            } else {
+                                $scope.mensagemUsuario = "Usuário ou Senha Inválidos";
+                            }
+
+                        });
+            };
         });
-   };
-   $scope.usuarioautent = {};
-   $scope.autenticarUsuario = function (){
-       $http.post('https://servicocontroleusuarios.herokuapp.com/usuario/autenticar',$scope.usuarioautent).
-        then(function(response) {
-            if(response.data){
-                $scope.mensagemUsuario = response.data;
-            }else{
-                $scope.mensagemUsuario = "Usuário ou Senha Inválidos";
-            }
-            
-        });
-   };  
-});
